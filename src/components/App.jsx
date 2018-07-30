fe16e64066c882aa78a5abe0f059419271f78df2
Richard Memory <richardfmemory@gmail.com>
2018-07-29 18:26:53 -0600

    Add some simple state and CSS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,38 @@
 /* eslint-disable react/jsx-indent-props, react/jsx-one-expression-per-line */
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 import './app.css';
 
-// const welcome = 'Welcome to React from Big Fish';
+class App extends Component {
+	state = {
+		isLit: true,
+	}
 
-const App = () => (
-	// <div className="App">
-	// 	<header className="App-header">
-	// 		<Welcome />
-	// 	</header>
-	// 	<p className="App-intro">
-	// 		{'To get started, edit'}
-	// 		<code>
-	// 			{'src/components/App.jsx'}
-	// 		</code>
-	// 		{'and save to reload.'}
-	// 	</p>
-	// </div>
-	<Fragment>
-		<MediaCard
-			title="How bout an apple?"
-			body={(
-				<em>
-					How do you like them apples?
-				</em>
-			)}
-			imageUrl="http://cjsmarket.com/wp-content/uploads/2015/11/Bushel-of-Apples-600x600.png"
-		/>
-		<hr />
-		<Gate
-			isOpen="true"
-		/>
-	</Fragment>
-);
+	flipLight = (event) => {
+		event.preventDefault();
+		this.setState({
+			isLit: !this.state.isLit,
+		});
+	}
 
-// const Welcome = () => (
-// 	<h1 className="App-title">
-// 		{welcome}
-// 	</h1>
-// );
+	render() {
+		const brightness = this.state.isLit ? 'lit' : 'dark';
+		return (
+			<div className={`room ${brightness}`}>
+				the room is {brightness}
+				<br />
+				<button type="submit" onClick={this.flipLight}>
+					flip
+				</button>
+			</div>
+		);
+	}
+}
 
-const imgStyle = {
-	width: '100px',
-	height: '100px',
-};
-
-const MediaCard = ({ title, body, imageUrl }) => (
-	<div className="media">
-		<img className="mr-3" style={imgStyle} src={imageUrl} alt="apples" />
-		<div className="media-body">
-			<h2>
-				{title}
-			</h2>
-			<p>
-				{body}
-			</p>
-		</div>
-	</div>
-);
-MediaCard.propTypes = {
-	title: PropTypes.string.isRequired,
-	body: PropTypes.string.isRequired,
-	imageUrl: PropTypes.string.isRequired,
-};
-
-const Gate = ({ isOpen }) => (
-	<p>
-		Gate is { isOpen
-			? <span className="alert alert-danger" role="alert">Open</span>
-			: <span className="alert alert-success" role="alert">Closed</span>
-		}
-	</p>
-);
-Gate.propTypes = {
-	isOpen: PropTypes.bool.isRequired,
-};
+// MediaCard.propTypes = {
+// 	title: PropTypes.string.isRequired,
+// 	body: PropTypes.string.isRequired,
+// 	imageUrl: PropTypes.string.isRequired,
+// };
 
 export default App;
