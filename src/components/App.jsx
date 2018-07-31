1d5fb2bfa3154ba034ba85fb47fd2b6ed3c267f5
Richard Memory <richardfmemory@gmail.com>
2018-07-30 18:05:06 -0600

    Add a simple example of accessing an API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,42 @@
 /* eslint-disable react/jsx-indent-props, react/jsx-one-expression-per-line */
 import React, { Component } from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types';
 import './app.css';
 
 class App extends Component {
 	state = {
-		isLit: true,
+		posts: [],
 	}
 
-	flipLight = (event) => {
-		event.preventDefault();
-		this.setState({
-			isLit: !this.state.isLit,
-		});
+	componentDidMount() {
+		axios.get('https://www.reddit.com/r/reactjs.json')
+			.then((res) => {
+				/* Drill into the response to extract the titles
+				   of the various posts which are found under
+				   res.data.data.children, which is a list of objects
+				   containing two fields: "kind" and "data". We only care
+				   about the "data" because it contains an object, which
+				   in turn contains the title. Thus the map operation
+				   returns an list containing the all of the obj.data's.
+				   These are cast by the application below to be a "post",
+				   from which we obtain the id and title. */
+				const posts = res.data.data.children.map(obj => obj.data);
+				this.setState({ posts });
+			});
 	}
 
 	render() {
-		const brightness = this.state.isLit ? 'lit' : 'dark';
 		return (
-			<div className={`room ${brightness}`}>
-				the room is {brightness}
-				<br />
-				<button type="submit" onClick={this.flipLight}>
-					flip
-				</button>
+			<div>
+				<h1>
+					/r/react.js
+				</h1>
+				<ul>
+					{this.state.posts.map(post => (
+						<li key={post.id}>{post.title}</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
